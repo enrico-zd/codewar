7f0957375f8b70e9d109d668a590bbdce217c8d1
Enrico Zada <enricozada@icloud.com>
2021-09-27 11:49:59 +0700

    revisi

--- Count_of_positives_and_sum_of_negatives.py
+++ Count_of_positives_and_sum_of_negatives.py
@@ -10,41 +10,22 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
 
 """
 
-# # cara sendiri
-# def coba(a):
-#     while True:
-#         x = 0
-#         y = 0
-#         for char in a:
-#             if char > 0:
-#                 x += 1
-#             elif char < 0:
-#                 y += char
-#         if a == []:
-#             return a
-#         else:
-#             return [x,y]
+# cara sendiri
+def coba(a):
+    while True:
+        x = 0
+        y = 0
+        for char in a:
+            if char > 0:
+                x += 1
+            elif char < 0:
+                y += char
+        if a == []:
+            return a
+        else:
+            return [x,y]
         
-# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
-# print(coba(a))
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
+print(coba(a))
 
-
-
-# # cara solusi 1
-# def count_positives_sum_negatives(arr):
-#     pos = sum(1 for x in arr if x > 0)
-#     neg = sum(x for x in arr if x < 0)
-#     return [pos, neg] if len(arr) else []
-
-
-
-# # cara solusi 2
-# def count_positives_sum_negatives(arr):
-#     return [len([x for x in arr if x > 0])] + [sum(y for y in arr if y < 0)] if arr else []
-
-
-
-# # cara solusi 3
-# def count_positives_sum_negatives(arr):
-#     return [sum(n > 0 for n in arr), sum(n for n in arr if n < 0)] if arr else []
 
--- Descending_Order.py
+++ Descending_Order.py
@@ -11,21 +11,17 @@ Input: 123456789 Output: 987654321
 
 """
 
-# # cara sendiri
-# def descending_order(num):
-#     angka = [int(x) for x in str(num)]
-#     data = len(angka)-1
-#     for i in range(data):
-#         for j in range(data):
-#             if angka[j] < angka[j+1]:
-#                 angka[j], angka[j+1] = angka[j+1], angka[j]
+# cara sendiri
+def descending_order(num):
+    angka = [int(x) for x in str(num)]
+    data = len(angka)-1
+    for i in range(data):
+        for j in range(data):
+            if angka[j] < angka[j+1]:
+                angka[j], angka[j+1] = angka[j+1], angka[j]
 
-#     s = [str(y) for y in angka]
-#     hasil = int(''.join(s))
-#     return hasil
+    s = [str(y) for y in angka]
+    hasil = int(''.join(s))
+    return hasil
 
-# print(descending_order(123456789))
-
-# # cara solusi
-# def Descending_Order(num):
-#     return int("".join(sorted(str(num), reverse=True)))
+print(descending_order(123456789))
